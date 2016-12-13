3dd10932563b58f483f7cf0c93d98fdad5534710
Bali <ali.ben.saad@gmail.com>
2016-12-13 23:20:19 +0100

    ajout register activity et registerFragment

---
+++ app/src/main/java/fr/sio/ecp/federatedbirds/app/RegisterActivity.java
@@ -0,0 +1,54 @@
+package fr.sio.ecp.federatedbirds.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import fr.sio.ecp.federatedbirds.R;
+import fr.sio.ecp.federatedbirds.utils.ValidationUtils;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+    }
+
+    private void register() {
+
+        // Get form views
+        EditText usernameText = (EditText) findViewById(R.id.username);
+        EditText passwordText = (EditText) findViewById(R.id.password);
+
+        String login = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (!ValidationUtils.validateLogin(login)) {
+            usernameText.setError(getString(R.string.invalid_format));
+            usernameText.requestFocus();
+            return;
+        }
+
+        if (!ValidationUtils.validatePassword(password)) {
+            passwordText.setError(getString(R.string.invalid_format));
+            passwordText.requestFocus();
+            return;
+        }
+
+        LoginTaskFragment taskFragment = new LoginTaskFragment();
+        taskFragment.setArguments(login, password);
+        taskFragment.show(getSupportFragmentManager(), "login_task");
+
+    }
+
+}
+
